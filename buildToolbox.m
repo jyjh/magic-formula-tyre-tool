e0143c9585b143e02d3cd4d59169ff84adc30c4f
jyjh <jerryyenjh@gmail.com>
2026-07-14 11:53:59 +0800

    buildToolbox.m

---
+++ buildToolbox.m
@@ -0,0 +1,20 @@
+opts = matlab.addons.toolbox.ToolboxOptions(pwd, 'MagicFormulaTyreTool');
+opts.ToolboxName = 'MagicFormulaTyreTool';
+opts.ToolboxVersion = '1.5.1';
+opts.Summary = 'MATLAB GUI for Magic Formula Tyre Modeling';
+opts.Description = 'https://github.com/teasit/magic-formula-tyre-tool';
+opts.ToolboxImageFile = fullfile(pwd, 'assets', 'img', 'App_Screenshot_Main.jpg');
+opts.ToolboxGettingStartedGuide = fullfile(pwd, 'doc', 'GettingStarted.mlx');
+opts.OutputFile = fullfile(pwd, 'MagicFormulaTyreTool.mltbx');
+opts.SupportedPlatforms.Win64 = true;
+opts.SupportedPlatforms.Maci64 = true;
+opts.SupportedPlatforms.Glnxa64 = true;
+opts.SupportedPlatforms.MatlabOnline = true;
+opts.ToolboxFiles = { ...
+    fullfile(pwd, 'CHANGELOG.md'), ...
+    fullfile(pwd, 'doc'), ...
+    fullfile(pwd, 'LICENSE'), ...
+    fullfile(pwd, 'MagicFormulaTyreTool.mlappinstall'), ...
+    fullfile(pwd, 'README.md') ...
+};
+matlab.addons.toolbox.packageToolbox(opts);
